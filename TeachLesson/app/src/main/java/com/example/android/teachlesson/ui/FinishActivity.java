700b9e4da77bba02a07ea494e9833e11ec255693
= <=>
2018-01-17 09:00:33 -0200

    Put the pontuation in the firebase
    Signed-off-by: = <=>

--- TeachLesson/app/src/main/java/com/example/android/teachlesson/ui/FinishActivity.java
+++ TeachLesson/app/src/main/java/com/example/android/teachlesson/ui/FinishActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.teachlesson.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.teachlesson.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import static com.example.android.teachlesson.ui.QuestionActivity.USER_PONTUATION;
 
@@ -23,6 +28,15 @@ public class FinishActivity extends AppCompatActivity {
     private TextView mTvPontuationText;
     private TextView mTvUserPontuation;
 
+    private FirebaseUser userFirebase;
+
+    private DatabaseReference mDatabase;
+
+    private String nameUser;
+    private String emailUser;
+    private Uri photoUriUser;
+    private String uidUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,28 @@ public class FinishActivity extends AppCompatActivity {
 
         mTvPontuationText = (TextView) findViewById(R.id.tv_pontuation);
         mTvUserPontuation = (TextView) findViewById(R.id.tv_userPontuation);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        userFirebase = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (userFirebase != null) {
+            nameUser = userFirebase.getDisplayName();
+            emailUser = userFirebase.getEmail();
+            photoUriUser = userFirebase.getPhotoUrl();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            uidUser = userFirebase.getUid();
+
+            System.out.println("PARABENS USUARIO :");
+            System.out.println(nameUser);
+            System.out.println(emailUser);
+            System.out.println(uidUser);
+
+
+        }
 
         updateUI();
     }
@@ -79,5 +115,25 @@ public class FinishActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
         finish();
         super.onBackPressed();
+    }
+
+
+    /**
+     * This method is called when the Save button is clicked. It will simply save
+     * the pontuation into Firebase
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickSavePontuation(View v) {
+
+        String pontuation =
+                mTvPontuationText.getText() + " " + userPontuation;
+
+        mDatabase.child("users").child(uidUser).child("name").setValue(nameUser);
+        mDatabase.child("users").child(uidUser).child("email").setValue(emailUser);
+        mDatabase.child("users").child(uidUser).child("photo").setValue(photoUriUser.toString());
+        mDatabase.child("users").child(uidUser).child("pontuation").setValue(String.valueOf(userPontuation));
+
+        System.out.println("alll");
     }
 }
